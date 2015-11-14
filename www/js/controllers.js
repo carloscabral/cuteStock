74f46c662dc989fe23531faef08f31286bc58ef3
Carlos Cabral <carlos.gomescabral@gmail.com>
2015-11-14 15:47:31 -0200

    Added Notes Functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -62,8 +62,8 @@ angular.module('cuteStock.controllers', [])
 
 }])
 
-.controller('StockCtrl', ['$scope', '$stateParams', '$window', 'stockDataService', 'dateService', 'chartDataService',
-  function($scope, $stateParams, $window, stockDataService, dateService, chartDataService) {
+.controller('StockCtrl', ['$scope', '$stateParams', '$window', '$ionicPopup', 'stockDataService', 'dateService', 'chartDataService', 'notesService',
+  function($scope, $stateParams, $window, $ionicPopup, stockDataService, dateService, chartDataService, notesService) {
 
     $scope.ticker = $stateParams.stockTicker;
     $scope.chartView = 4;
@@ -74,11 +74,81 @@ angular.module('cuteStock.controllers', [])
       getPriceData();
       getDetailsData();
       getChartData();
+      $scope.stockNotes = notesService.getNotes($scope.ticker);
     });
 
     $scope.chartViewFunc = function (n) {
       $scope.chartView = n;
     };
+
+    $scope.addNote = function() {
+
+      $scope.note = {title: 'Note', body: '', date: $scope.todayDate, ticker: $scope.ticker};
+
+      var note = $ionicPopup.show({
+        template: '<input type="text" ng-model="note.title" id="stock-note-title"><textarea type="text" ng-model="note.body" id="stock-note-body"></textarea>',
+        title: 'New Note for ' + $scope.ticker,
+        scope: $scope,
+        buttons: [
+          {
+            text: 'Cancel',
+            onTap: function (e) {
+              return;
+            }
+          },
+
+          {
+            text: '<b>Save</b>',
+            type: 'button-balanced',
+            onTap: function(e) {
+              notesService.addNote($scope.ticker, $scope.note);
+            }
+          }
+        ]
+      });
+      note.then(function(res) {
+        $scope.stockNotes = notesService.getNotes($scope.ticker);
+      });
+    };
+
+    $scope.openNote = function(index, title, body) {
+      $scope.note = {title: title, body: body, date: $scope.todayDate, ticker: $scope.ticker};
+
+      var note = $ionicPopup.show({
+        template: '<input type="text" ng-model="note.title" id="stock-note-title"><textarea type="text" ng-model="note.body" id="stock-note-body"></textarea>',
+        title: $scope.note.title,
+        scope: $scope,
+        buttons: [
+          {
+            text: 'Delete',
+            type: 'button-assertive button-small',
+            onTap: function(e) {
+              notesService.deleteNote($scope.ticker, index);
+            }
+          },
+          {
+            text: 'Cancel',
+            type: 'button-small',
+            onTap: function(e) {
+              return;
+            }
+           },
+          {
+            text: '<b>Save</b>',
+            type: 'button-balanced button-small',
+            onTap: function(e) {
+              notesService.deleteNote($scope.ticker, index);
+              notesService.addNote($scope.ticker, $scope.note);
+            }
+          }
+        ]
+      });
+
+      note.then(function(res) {
+        $scope.stockNotes = notesService.getNotes($scope.ticker);
+      });
+    };
+
 
     function getPriceData() {
       var promise = stockDataService.getPriceData($scope.ticker);
--- www/js/services.js
+++ www/js/services.js
@@ -80,6 +80,23 @@ angular.module('cuteStock.services', [])
 })
 
 
+.factory('notesCacheService', function(CacheFactory) {
+
+  var notesCache;
+
+  if(!CacheFactory.get('notesCache')) {
+    notesCache = CacheFactory('notesCache', {
+      storageMode: 'localStorage'
+    });
+  }
+  else {
+    notesCache = CacheFactory.get('notesCache');
+  }
+
+  return notesCache;
+})
+
+
 .factory('stockDataService', function ($q, $http, encodeURIService, stockDetailsCacheService) {
 
 	var getDetailsData = function(ticker) {
@@ -218,6 +235,41 @@ angular.module('cuteStock.services', [])
 		getHistoricalData: getHistoricalData
 	};
 
+})
+
+
+.factory('notesService', function(notesCacheService) {
+
+  return {
+
+    getNotes: function(ticker) {
+      return notesCacheService.get(ticker);
+    },
+
+    addNote: function(ticker, note) {
+
+      var stockNotes = [];
+
+      if(notesCacheService.get(ticker)) {
+        stockNotes = notesCacheService.get(ticker);
+        stockNotes.push(note);
+      }
+      else {
+        stockNotes.push(note);
+      }
+
+      notesCacheService.put(ticker, stockNotes);
+    },
+
+    deleteNote: function(ticker, index) {
+
+      var stockNotes = [];
+
+      stockNotes = notesCacheService.get(ticker);
+      stockNotes.splice(index, 1);
+      notesCacheService.put(ticker, stockNotes);
+    }
+  };
 })
 
 ;
